ffdf321da3d3200c46176235235132a1eae4770c
kaushal singh <kaushal.singh1@globallogic.com>
2022-01-06 19:13:27 +0530

    api gateway

---
+++ src/test/java/com/gl/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
